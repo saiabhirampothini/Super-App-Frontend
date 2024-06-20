980229d011e5e2f744a521bee2e7c82256253dd6
saiabhirampothini <saiabhirampothini@gmail.com>
2024-06-20 19:06:31 +0530

    Route-Protection

--- src/components/Protect.js
+++ src/components/Protect.js
@@ -1,35 +1,34 @@
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios";
-// import {checkRoute} from "../utils/APIRoutes"
-// const ProtectedRoute = ({ children }) => {
-//   const navigate = useNavigate();
-//   const checkFunction = async () => {
-//     try {
-//       const response = await axios.get(checkRoute, {
-//         withCredentials: true,
-//       });
-//       console.log(response)
-//       if (response.status === 200) {
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     } catch (err) {
-//       if (err.response.status === 400) {
-//         // console.log("error");
-//         return navigate("/login");
-//       }
-//     }
-//   };
-//   let check = checkFunction();
-//   if (check && children) return children;
-//   return navigate("/login");
-// };
-// export default ProtectedRoute;
-
-
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { checkRoute } from "../utils/APIRoutes";
 const ProtectedRoute = ({ children }) => {
-  return children;
+  const navigate = useNavigate();
+  const checkFunction = async () => {
+    try {
+      const response = await axios.get(checkRoute, {
+        withCredentials: true,
+      });
+      console.log(response);
+      if (response.status === 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      if (err.response.status === 400) {
+        // console.log("error");
+        return navigate("/login");
+      }
+    }
+  };
+  let check = checkFunction();
+  if (check && children) return children;
+  return navigate("/login");
 };
 export default ProtectedRoute;
+
+// import { useNavigate } from "react-router-dom";
+// const ProtectedRoute = ({ children }) => {
+//   return children;
+// };
+// export default ProtectedRoute;
--- src/components/Unprotected.js
+++ src/components/Unprotected.js
@@ -1,17 +1,16 @@
-// import { Navigate, useNavigate } from "react-router-dom";
-// const UnprotectedRoute = ({ children }) => {
-//   const navigate = useNavigate();
-//   if (localStorage.getItem("profile")) {
-//     return <Navigate to="/dashBoard" replace />;
-//   } else {
-//     return children;
-//   }
-// };
-// export default UnprotectedRoute;
-
-
 import { Navigate, useNavigate } from "react-router-dom";
 const UnprotectedRoute = ({ children }) => {
-  return children;
+  const navigate = useNavigate();
+  if (localStorage.getItem("profile")) {
+    return <Navigate to="/dashBoard" replace />;
+  } else {
+    return children;
+  }
 };
 export default UnprotectedRoute;
+
+// import { Navigate, useNavigate } from "react-router-dom";
+// const UnprotectedRoute = ({ children }) => {
+//   return children;
+// };
+// export default UnprotectedRoute;
