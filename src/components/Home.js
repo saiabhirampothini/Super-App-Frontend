3f088eaf1ba21a926338d2b7e6a8cdc65a06f967
saiabhirampothini <saiabhirampothini@gmail.com>
2024-06-21 18:00:26 +0530

    Home-Page-Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,26 @@
+import "../styles/home.css";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="home-App">
+      <div className="home-container">
+        <h1 className="home-title">Super App</h1>
+        <p className="home-subtitle">Your one-stop solution for everything</p>
+        <div className="home-button-container">
+          <button className="home-button">
+            <Link className="nav-link" to="/login">
+              LOGIN
+            </Link>
+          </button>
+          <button className="home-button">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
